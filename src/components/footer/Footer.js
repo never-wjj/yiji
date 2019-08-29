40455543ed575fa823a5719005a2c9dcc641d76b
unknown <never-wjj>
2019-08-29 14:19:37 +0800

    aaaa

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { HashRouter as Router } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./footer.scss";
 class Footer extends Component {
   state = {};
@@ -8,31 +8,23 @@ class Footer extends Component {
     return (
       <footer>
         <Router>
-          <Link to="/home">
-            <img
-              src="https://dev.tencent.com/u/never-wjj/p/dev-demo/git/raw/master/icon__zhan__2_u140.png"
-              alt=""
-            />
-          </Link>
+          <NavLink exact to="/home" activeClassName="active">
+            <span className="iconfont icon-peiwangyindao" />
+            <p>展讯</p>
+          </NavLink>
 
-          <Link to="/home/homeOne">
-            <img
-              src="https://dev.tencent.com/u/never-wjj/p/dev-demo/git/raw/master/icon__tui_u132.png"
-              alt=""
-            />
-          </Link>
-          <Link to="/home/homeTow">
-            <img
-              src="https://dev.tencent.com/u/never-wjj/p/dev-demo/git/raw/master/icon__fa_u134.png"
-              alt=""
-            />
-          </Link>
-          <Link to="/home/homeThree">
-            <img
-              src="https://dev.tencent.com/u/never-wjj/p/dev-demo/git/raw/master/icon__ge_u136.png"
-              alt=""
-            />
-          </Link>
+          <NavLink to="/home/homeOne">
+            <span className="iconfont icon-yunyingguanli" />
+            <p>推荐</p>
+          </NavLink>
+          <NavLink to="/home/homeTow">
+            <span className="iconfont icon-yunduanshangchuan" />
+            <p>发布</p>
+          </NavLink>
+          <NavLink to="/home/homeThree">
+            <span className="iconfont icon-icon_zhanghao" />
+            <p>个人</p>
+          </NavLink>
         </Router>
       </footer>
     );
