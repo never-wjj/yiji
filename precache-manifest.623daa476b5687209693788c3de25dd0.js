ad4bd27a7bb1df11690fca6672865ca41ad3f1f9
unknown <never-wjj>
2019-08-29 11:01:33 +0800

    ssss

---
+++ precache-manifest.623daa476b5687209693788c3de25dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7110e1b4bc890e714bca19ec45d285",
+    "url": "/yiji/index.html"
+  },
+  {
+    "revision": "a3701b78dd98c5826107",
+    "url": "/yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "68e91e06f29543cdf9ed",
+    "url": "/yiji/static/css/main.e6f3ee52.chunk.css"
+  },
+  {
+    "revision": "a3701b78dd98c5826107",
+    "url": "/yiji/static/js/2.16b70927.chunk.js"
+  },
+  {
+    "revision": "68e91e06f29543cdf9ed",
+    "url": "/yiji/static/js/main.f7c8b42f.chunk.js"
+  },
+  {
+    "revision": "2d8b19c7d1f4f192b58a",
+    "url": "/yiji/static/js/runtime~main.5a7f6ba9.js"
+  }
+]);
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import goin from './reducers/goin'
-const rootState = combineReducers({ goin })
-const store = createStore(rootState, applyMiddleware(thunk))
-export default store
